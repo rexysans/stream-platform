1887e6ca8fcf14e5920a7cd966ebd31642b0298c
RexySans~ <ariif00vi@gmail.com>
2025-12-22 16:33:19 +0530

    Implement lease-based worker claiming and recovery

--- worker/index.js
+++ worker/index.js
@@ -15,7 +15,9 @@ async function runWorker() {
   try {
     const result = await client.query(`
       UPDATE videos
-      SET status = 'processing'
+      SET status = 'processing',
+      claimed_at = NOW()
+
       WHERE id = (
         SELECT id
         FROM videos
@@ -35,8 +37,49 @@ async function runWorker() {
     console.error("worker error", err);
   } finally {
     client.release();
-    await pool.end();
   }
 }
 
-runWorker();
+async function detectAbandonedJobs() {
+  const client = await pool.connect();
+
+  try {
+    const result = await client.query(`
+            UPDATE videos
+          SET
+            status = 'uploaded',
+            claimed_at = NULL
+          WHERE id IN (
+            SELECT id
+            FROM videos
+            WHERE
+              status = 'processing'
+              AND claimed_at IS NOT NULL
+              AND NOW() - claimed_at > INTERVAL '1 minute'
+            LIMIT 1
+          )
+          RETURNING id;
+
+    `);
+
+    // console.log(result);
+
+    if (result.rowCount === 0) {
+      console.log("no abandoned jobs detected");
+    } else {
+      console.log("abandoned jobs detected and is freed", result.rows[0].id);
+    }
+  } catch (err) {
+    console.error("worker error", err);
+  } finally {
+    client.release();
+  }
+}
+
+async function main() {
+  await detectAbandonedJobs();
+  await runWorker();
+  await pool.end();
+}
+
+main().catch(console.error);
